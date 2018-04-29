2a5175e1bec16783fc8c6d434dff61bbeda2d05f
maayan <maayan@marketing66.com>
2018-04-29 13:17:05 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Symfony
+=======
+
+A Symfony project created on April 29, 2018, 8:41 am.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+    /**
+     * @Route("/cryptocurrencies", name="cryptocurrencies")
+     */
+    public function cryptocurrenciesAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/cryptocurrencies.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+
+
+
+    /**
+     * @Route("/forex", name="forex")
+     */
+    public function forexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/forex.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
