f87d92d1d7b2a995a9fef293a18fab3c2578dbac
ortal <ortal@marketing66.com>
2018-07-26 18:33:59 +0300

    message

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -119,7 +119,7 @@ class DefaultController extends Controller
     public function TermsAction()
     {
         // replace this example code with whatever you need
-        return $this->render('default/Term&&Conditions.html.twig');
+        return $this->render('default/Term&Conditions.html.twig');
     }
 
     /**
