b33fea2e1b0f0e8547600441112f0fc7c7bdbb46
ortal <ortal@marketing66.com>
2018-08-14 17:39:14 +0300

    message

--- src/AppBundle/Controller/SitemapController.php
+++ src/AppBundle/Controller/SitemapController.php
@@ -28,10 +28,22 @@ class SitemapController extends Controller
         $urls = [];
         $hostname = $request->getHost();
 
-        $urls[] = ['loc' => $this->get('router')->generate('crypto'), 'changefreq' => 'weekly', 'priority' => '1.0'];
-        $urls[] = ['loc' => $this->get('router')->generate('forex'), 'changefreq' => 'weekly', 'priority' => '1.0'];
-        $urls[] = ['loc' => $this->get('router')->generate('commodities'), 'changefreq' => 'weekly', 'priority' => '1.0'];
-        $urls[] = ['loc' => $this->get('router')->generate('stock'), 'changefreq' => 'weekly', 'priority' => '1.0'];
+        $urls[] = ['loc' => $this->get('router')->generate('crypto'), 'changefreq' => 'weekly', 'priority' => '0.5'];
+        $urls[] = ['loc' => $this->get('router')->generate('forex'), 'changefreq' => 'weekly', 'priority' => '0.5'];
+        $urls[] = ['loc' => $this->get('router')->generate('commodities'), 'changefreq' => 'weekly', 'priority' => '0.5'];
+        $urls[] = ['loc' => $this->get('router')->generate('stock'), 'changefreq' => 'weekly', 'priority' => '0.5'];
+
+        $urls[] = ['loc' => $this->get('router')->generate('etoro'), 'changefreq' => 'weekly', 'priority' => '0.6'];
+        $urls[] = ['loc' => $this->get('router')->generate('24option'), 'changefreq' => 'weekly', 'priority' => '0.6'];
+        $urls[] = ['loc' => $this->get('router')->generate('trade'), 'changefreq' => 'weekly', 'priority' => '0.6'];
+        $urls[] = ['loc' => $this->get('router')->generate('plus500'), 'changefreq' => 'weekly', 'priority' => '0.6'];
+        $urls[] = ['loc' => $this->get('router')->generate('pepperstone'), 'changefreq' => 'weekly', 'priority' => '0.6'];
+        $urls[] = ['loc' => $this->get('router')->generate('alvexo'), 'changefreq' => 'weekly', 'priority' => '0.6'];
+        $urls[] = ['loc' => $this->get('router')->generate('market'), 'changefreq' => 'weekly', 'priority' => '0.6'];
+
+        $urls[] = ['loc' => $this->get('router')->generate('Live_rates_stocks'), 'changefreq' => 'weekly', 'priority' => '0.7'];
+        $urls[] = ['loc' => $this->get('router')->generate('Live_rates_crypto'), 'changefreq' => 'weekly', 'priority' => '0.7'];
+        $urls[] = ['loc' => $this->get('router')->generate('Live_rates_forex'), 'changefreq' => 'weekly', 'priority' => '0.7'];
 
         return $this->render('sitemap.xml.twig', [
             'urls' => $urls,
