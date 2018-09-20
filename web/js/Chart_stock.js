cd26c4e93c7e7809d244e20e059a6849e0618a4a
ortal <ortal@marketing66.com>
2018-09-20 15:46:11 +0300

    message

--- web/js/Chart_stock.js
+++ web/js/Chart_stock.js
@@ -1,12 +1,6 @@
 
-// (function(window) {
 var Chart_stockApp = angular.module('Chart_stockApp', []).config(function ($interpolateProvider) {
     $interpolateProvider.startSymbol('{[{').endSymbol('}]}');})
-
-
-// var demo = angular.module('LiveFeedsApp', []).config(function ($interpolateProvider) {
-//     $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
-// });
 
 Chart_stockApp.controller("Chart_stockController", function ($scope, $http) {
 
@@ -20,28 +14,29 @@ Chart_stockApp.controller("Chart_stockController", function ($scope, $http) {
 
     var i = 0
     $scope.init = function (api, currency,likes) {
-         console.log("api", api, "currency",currency)
+         //console.log("api", api, "currency",currency)
+        console.log("currency",currency)
+
 
         $.ajax({
-            url: api,
+            url: "https://websocket-stock.herokuapp.com/getStockPrice/" +  currency,
             type: "GET",
             success: function (result) {
-                //console.log("result $scope.all1",result)
-                $scope.stocks = result
-                console.log(" $scope.stocks", $scope.stocks)
+                console.log("result ***",result)
+                $scope.mystock = result[0]
+                // $scope.stocks = result
 
-                $scope.all.push($scope.stocks)
-                console.log(" $scope.all", $scope.all)
-
-                for (key in  $scope.stocks) {
-                    //console.log($scope.all1[key].name)
-                    if ($scope.stocks[key].name == currency) {
-                        //console.log("*****")
-                        $scope.mystock = result[key]
-                        console.log("*****",$scope.mystock )
-                        //console.log("api************found", from, to,$scope.mycrypto )
-                    }
-                }
+                // $scope.all.push($scope.stocks)
+                // console.log(" $scope.all", $scope.all)
+                //
+                // for (key in  $scope.stocks) {
+                //
+                //     if ($scope.stocks[0].symbol == currency) {
+                //         $scope.mystock = result[0]
+                         console.log("*****",$scope.mystock )
+                //         //console.log("api************found", from, to,$scope.mycrypto )
+                //     }
+                // }
 
                 $scope.$apply()
             },
@@ -51,45 +46,37 @@ Chart_stockApp.controller("Chart_stockController", function ($scope, $http) {
             }
         });
 
-// var allimg={}
-
-        // console.log("img", img)
-        //
         // $.ajax({
-        //     url: img,
+        //     url: api,
         //     type: "GET",
         //     success: function (result) {
-        //         $scope.images = result
-        //         console.log("result $scope.images",  $scope.images)
-        //         for (var i = 0; i < $scope.images.length; i++) {
-        //             if($scope.images[i].symbol == currency)
-        //             {   $scope.allimg[$scope.images[i].symbol] = {img: $scope.images[i].img}
-        //                 console.log("$scope.allimg",$scope.allimg)
-        //                 //$scope.myimage =  $scope.images[i].img
-        //                 $scope.myname = $scope.images[i].name
-        //                 //console.log("$scope.allimg", $scope.allimg["FB"].img)
-        //                 console.log("$scope.myname",$scope.myname)
+        //         //console.log("result $scope.all1",result)
+        //         $scope.stocks = result
+        //         console.log(" $scope.stocks", $scope.stocks)
+        //
+        //         $scope.all.push($scope.stocks)
+        //         console.log(" $scope.all", $scope.all)
+        //
+        //         for (key in  $scope.stocks) {
+        //             //console.log($scope.all1[key].name)
+        //             if ($scope.stocks[key].pair == currency) {
+        //                 //console.log("*****")
+        //                 $scope.mystock = result[key]
+        //                 console.log("*****",$scope.mystock )
+        //                 //console.log("api************found", from, to,$scope.mycrypto )
         //             }
-        //
-        //
         //         }
-        //         // for (const key in myobj) {
-        //         //     if (myobj.hasOwnProperty(key)) {
-        //         //         const element = myobj[key];
-        //         //         $scope.allimg.push(element)
-        //         //     }}
-        //         //  console.log("Response------", myobj)
         //
         //         $scope.$apply()
         //     },
+        //
         //     error: function (xhr, ajaxOptions, thrownError) {
         //         console.log("ERROR", thrownError, xhr, ajaxOptions)
         //     }
-        //
         // });
 
         ////////////////////////////////////////////////////////////////////
-        console.log("likes", likes)
+        //console.log("likes", likes)
         $.ajax({
             url: likes,
             type: "GET",
@@ -110,7 +97,7 @@ Chart_stockApp.controller("Chart_stockController", function ($scope, $http) {
 
                 }
 
-                console.log("Response*likes*", $scope.allimg)
+                //console.log("Response*likes*", $scope.allimg)
 
                 $scope.$apply()
 
@@ -123,35 +110,6 @@ Chart_stockApp.controller("Chart_stockController", function ($scope, $http) {
 
     }
 
-
-    //
-    // var socket = io.connect("https://websocket-stock.herokuapp.com")
-    //
-    // socket.on('connect', function () {
-    //     socket.emit('room', "persec");
-    //     socket.on('message', data => {
-    //         //console.log("STOCK RESP", data)
-    //         for (const key in data) {
-    //         var item73 = $scope.all.find(function (element) {
-    //
-    //             return (element[0][key].name == data[key].name);
-    //
-    //         })
-    //         if (typeof item73 != typeof undefined) {
-    //             for (const ky in data[key]) {
-    //                 if (data.hasOwnProperty(key)) {
-    //                     item73[ky] = data[key][ky];
-    //
-    //                 }
-    //             }
-    //
-    //         }
-    //         $scope.$apply()
-    //     }
-    //     //
-    // })
-    // })
-
 });
 
 var dvChart_stock = document.getElementById('dvChart_stock');
@@ -160,7 +118,6 @@ angular.element(document).ready(function() {
 
     angular.bootstrap(dvChart_stock, ['Chart_stockApp']);
 });
-//
-// })(window);
+
 
 
