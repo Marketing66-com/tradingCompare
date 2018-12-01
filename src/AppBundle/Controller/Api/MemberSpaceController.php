d6a1e0b1730ee3ae4ffd5c5f9f5e547400fa92c3
Evis Bregu <evis.bregu@gmail.com>
2018-12-01 21:14:06 +0100

    Testing API client

--- src/AppBundle/Controller/Api/MemberSpaceController.php
+++ src/AppBundle/Controller/Api/MemberSpaceController.php
@@ -4,6 +4,7 @@
 namespace AppBundle\Controller\Api;
 
 
+use AppBundle\SDK\ApiClient;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -13,9 +14,10 @@ class MemberSpaceController extends BaseController
     /**
      * @Security("is_granted('ROLE_USER')")
      * @Route("/secured", name="secured", options={"i18n"=false})
+     * @throws \Exception
      */
     public function secureSpaceAction()
     {
-        return new JsonResponse(['OK']);
+        return new JsonResponse($this->get(ApiClient::class)->getUsers());
     }
 }
