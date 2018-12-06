75d4f1bfaa53f9cb40c5ef2a6552fc49113b4129
Evis Bregu <evis.bregu@gmail.com>
2018-12-06 11:45:39 +0100

    Remove debug code

--- web/modules/sample/js/script.js
+++ web/modules/sample/js/script.js
@@ -1,9 +1,4 @@
 const app = angular.module('app', []);
-//   .config(function ($interpolateProvider) {
-//   $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
-// });
-
-//app.value('firebaseObject', firebase);
 
 app.controller('AppCtrl', function ($scope, $http, $location) {
   $scope.firebase = firebase;
@@ -11,21 +6,17 @@ app.controller('AppCtrl', function ($scope, $http, $location) {
   $scope.data = {};
 
   this.$onInit = function () {
-    console.log('INIT');
-
     firebase.auth().onAuthStateChanged(function (user) {
       if (user) {
         user.getIdToken(/* forceRefresh */ true).then(function (idToken) {
-          console.log('TOKEN', idToken);
           const url = `${$location.protocol()}://${$location.host()}:${$location.port()}/api/secured`;
-          console.log('URL', url);
+
           $http.defaults.headers.common.Authorization = `Bearer ${idToken}`;
           $http.get(url).then(function (response) {
             $scope.data = response.data;
             $scope.apply();
           });
         }).catch(function (error) {
-          console.log('ERROR');
           $scope.data = error;
           $scope.apply();
         });
