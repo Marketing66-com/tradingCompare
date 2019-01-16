0f373b9f16e1582576cde23ac0e35e77c8f3a59d
ortal <ortal@marketing66.com>
2019-01-16 09:30:56 +0200

    message

--- src/ChartiqBundle/Controller/ChartiqController.php
+++ src/ChartiqBundle/Controller/ChartiqController.php
@@ -7,25 +7,25 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class ChartiqController extends Controller
 {
-    /**
-     * @Route("/chartiq", name="chartiq")
-     **/
-    public function indexAction()
-    {
-        return $this->render('ChartiqBundle:Default:sample-template-advanced.html.twig');
-    }
-
-
-    /**
-     * @Route("/live-chart", name="live-chart")
-     **/
-    public function liveChartAction()
-    {
-        $crypto_api =  $this->getParameter('crypto_api');
-
-        return $this->render('ChartiqBundle:Default:test-chart.html.twig', array("currency"=>'BTC', "from"=>'BTC', "to"=>'USD', "crypto_api"=>$crypto_api,
-            "name"=>'Bitcoin', "symbol"=>'BTC_USD'));
-
-        //return $this->render('ChartiqBundle:Default:index.html.twig');
-    }
+//    /**
+//     * @Route("/chartiq", name="chartiq")
+//     **/
+//    public function indexAction()
+//    {
+//        return $this->render('ChartiqBundle:Default:sample-template-advanced.html.twig');
+//    }
+//
+//
+//    /**
+//     * @Route("/live-chart", name="live-chart")
+//     **/
+//    public function liveChartAction()
+//    {
+//        $crypto_api =  $this->getParameter('crypto_api');
+//
+//        return $this->render('ChartiqBundle:Default:test-chart.html.twig', array("currency"=>'BTC', "from"=>'BTC', "to"=>'USD', "crypto_api"=>$crypto_api,
+//            "name"=>'Bitcoin', "symbol"=>'BTC_USD'));
+//
+//        //return $this->render('ChartiqBundle:Default:index.html.twig');
+//    }
 }
